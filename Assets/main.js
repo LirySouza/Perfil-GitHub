defd8177f551a602ddbf66480a9458dc007fabdc
LirySouza <lirianysouza15@gmail.com>
2023-08-04 10:32:52 -0300

    Java Funcionando

--- Assets/main.js
+++ Assets/main.js
@@ -1,12 +1,3 @@
-document.addEventListener("click" , trocaCor);
-
-document.getElementById( "Btn" ).addEventListener( "click");
-
-function mudar( e )
-{
-    let nome = document.getElementById( "mudar" ).value;
-    e.preventDefault();
-}
-function trocaCor( e ){
+function Botao(){
     document.body.style.backgroundColor = 'pink';
 }
